e55cd3ddd600c2beb8e15477262ab03ce4f2dd63
Samir <samirb80440@gmail.com>
2024-04-23 08:52:52 +0200

    nvmodif

---
+++ Fil rouge/Js/redirection.js
@@ -0,0 +1,26 @@
+function redirection (cat){
+    if(cat==1){
+        window.location.href="categorie/asianfood.html"
+    }
+    if(cat==2){
+        window.location.href="categorie/hamburger.html"
+    }
+    if(cat==3){
+        window.location.href="categorie/pasta.html"
+    }
+    if(cat==4){
+        window.location.href="categorie/pizza.html"
+    }
+    if(cat==5){
+        window.location.href="categorie/salade.html"
+    }
+    if(cat==6){
+        window.location.href="categorie/sandwich.html"
+    }
+    if(cat==7){
+        window.location.href="categorie/veggie.html"
+    }
+    if(cat==8){
+        window.location.href="categorie/wrap.html"
+    }
+}

33cea0c94e8edb2d8b3c8f6e49d4c7a74f34ab91
Samir <samirb80440@gmail.com>
2024-04-24 09:16:19 +0200

    nvmodif

--- Fil rouge/Js/carouselplat1.js
+++ Fil rouge/Js/carouselplat1.js
@@ -5,15 +5,16 @@ var hamburger = document.getElementById('pagehamburger')
 var pasta = document.getElementById('pagepate')
 var wrap = document.getElementById('pagewrap')
 var pizza = document.getElementById('pagepizza')
+var asian =document.getElementById('pageasian')
 var page = 1 
 hamburger.style.display= 'none'
 pasta.style.display= 'none'
 wrap.style.display= 'none'
 pizza.style.display= 'none'
-
+asian.style.display='none'
 suivant.addEventListener('click',function suivant(){
     page++
-    if (page > 5) {
+    if (page > 6) {
         page = 1
     }
     pages(page)
@@ -23,12 +24,14 @@ suivant.addEventListener('click',function suivant(){
 precedent.addEventListener('click',function precedent(){
     page--
     if ( page < 1) {
-        page = 5
+        page = 6
     }
     pages(page)
 })
 
+
 function pages(page){
+  asian.style.display='none'
 if (page==1) {
     veggie.style.display= 'block'
     hamburger.style.display= 'none'
@@ -38,19 +41,28 @@ if (page==2) {
     veggie.style.display= 'none'
     hamburger.style.display= 'block'
     pasta.style.display= 'none'
+    
 }
 if (page==3) {
     hamburger.style.display= 'none'
     pasta.style.display= 'block'
     wrap.style.display= 'none'
+    
 }
 if (page==4) {
     pasta.style.display='none'
     wrap.style.display='block'
     pizza.style.display='none'
+    
 }
 if (page==5) {
     veggie.style.display= 'none'
     wrap.style.display='none'
     pizza.style.display='block'
+   
+}
+if(page==6){
+  wrap.style.display='none'
+  pizza.style.display='none'
+  asian.style.display='block'
 }}

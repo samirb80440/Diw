8e32f4f3b897cb8d59c19d8b40173e86a22ece36
Samir <samirb80440@gmail.com>
2024-04-24 12:23:53 +0200

    nvmodif

--- Fil rouge/Js/carouselplat1.js
+++ Fil rouge/Js/carouselplat1.js
@@ -1,68 +1,66 @@
-var precedent = document.getElementById("precedent")//toutes mes div
-var suivant = document.getElementById("suivant")
-var veggie=document.getElementById('pageveggie')
-var hamburger = document.getElementById('pagehamburger')
-var pasta = document.getElementById('pagepate')
-var wrap = document.getElementById('pagewrap')
-var pizza = document.getElementById('pagepizza')
-var asian =document.getElementById('pageasian')
-var page = 1 
-hamburger.style.display= 'none'
-pasta.style.display= 'none'
-wrap.style.display= 'none'
-pizza.style.display= 'none'
-asian.style.display='none'
-suivant.addEventListener('click',function suivant(){
-    page++
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM fully loaded and parsed");
+  var precedent = document.getElementById("precedent");
+  var suivant = document.getElementById("suivant"); //definie les bouton Precedent et suivant
+
+  var veggie = document.getElementById("pageveggie");
+  var hamburger = document.getElementById("pagehamburger");
+  var pasta = document.getElementById("pagepate");
+  var wrap = document.getElementById("pagewrap");
+  var pizza = document.getElementById("pagepizza");
+  var asianfood = document.getElementById("pageasian");
+  var page = 1; //Compteur de Page pour le carousel
+  if (document.getElementById("checkplathtml") != null) {
+    hamburger.style.display = "none";
+    pasta.style.display = "none";
+    wrap.style.display = "none";
+    pizza.style.display = "none";
+    asianfood.style.display = "none";
+  } //definie et desactive tout les cellule sauf la premiere si checkplat existe
+
+  suivant.addEventListener("click", function suivant() {
+    page++;
     if (page > 6) {
-        page = 1
+      page = 1;
     }
-    pages(page)
-}
-)
+    pages(page);
+  }); //fait tourner le carousel avec Suivant
 
-precedent.addEventListener('click',function precedent(){
-    page--
-    if ( page < 1) {
-        page = 6
+  precedent.addEventListener("click", function precedent() {
+    page--;
+    if (page < 1) {
+      page = 6;
     }
-    pages(page)
-})
+    pages(page);
+  }); //fait tourner le carousel avec Precedent
 
+  function pages(page) {
+    //fonctionne pour afficher la page en fonction du compteur
 
-function pages(page){
-  asian.style.display='none'
-if (page==1) {
-    veggie.style.display= 'block'
-    hamburger.style.display= 'none'
-    pizza.style.display='none'
-}
-if (page==2) {
-    veggie.style.display= 'none'
-    hamburger.style.display= 'block'
-    pasta.style.display= 'none'
-    
-}
-if (page==3) {
-    hamburger.style.display= 'none'
-    pasta.style.display= 'block'
-    wrap.style.display= 'none'
-    
-}
-if (page==4) {
-    pasta.style.display='none'
-    wrap.style.display='block'
-    pizza.style.display='none'
-    
-}
-if (page==5) {
-    veggie.style.display= 'none'
-    wrap.style.display='none'
-    pizza.style.display='block'
-   
-}
-if(page==6){
-  wrap.style.display='none'
-  pizza.style.display='none'
-  asian.style.display='block'
-}}
+    veggie.style.display = "none";
+    hamburger.style.display = "none";
+    pasta.style.display = "none";
+    wrap.style.display = "none";
+    pizza.style.display = "none";
+    asianfood.style.display = "none";
+
+    if (page == 1) {
+      veggie.style.display = "block";
+    }
+    if (page == 2) {
+      hamburger.style.display = "block";
+    }
+    if (page == 3) {
+      pasta.style.display = "block";
+    }
+    if (page == 4) {
+      wrap.style.display = "block";
+    }
+    if (page == 5) {
+      pizza.style.display = "block";
+    }
+    if (page == 6) {
+      asianfood.style.display = "block";
+    }
+  }
+});

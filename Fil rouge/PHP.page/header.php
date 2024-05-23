93ba9706f5ef50e89815a4c2de31dcfbe893bf57
Samir <samirb80440@gmail.com>
2024-05-23 08:33:59 +0200

    nvmodif

---
+++ Fil rouge/PHP.page/header.php
@@ -0,0 +1,27 @@
+<header id="navbar">
+        <nav class="navbar navbar-expand-sm bg-danger navbar-dark">
+            <a class="navbar-brand" href="#"><img src="/images_the_district/the_district_brand/logo_transparent.png"
+                    width="50" class="d-inline-block align-text-top" alt="navicon"></a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse justify-content-center col-12" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                    <li class="textnav nav-item px-5">
+                        <a class="nav-link active" href="#" title="Accueil">Accueil</a>
+                    </li>
+                    <li class="textnav nav-item px-5">
+                        <a class="nav-link" href="/Categorie.html" title="Categorie">Categorie</a>
+                    </li>
+                    <li class="textnav nav-item px-5">
+                        <a class="nav-link" href="/Plat.html" title="Plat">Plat</a>
+                    </li>
+                    <li class="textnav nav-item px-5">
+                        <a class="nav-link" href="/contact.html" title="Contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>

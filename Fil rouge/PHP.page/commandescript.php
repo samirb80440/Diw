f27b03ec02c1fd10e872fe0488e4adb371f032d0
Samir <samirb80440@gmail.com>
2024-06-07 08:30:37 +0200

    nvmodif

--- Fil rouge/PHP.page/commandescript.php
+++ Fil rouge/PHP.page/commandescript.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
 
-echo 'la livraison de votre commande est estimer a  '.date('H:i:s', strtotime('+30 minutes', strtotime(date('H:i:s'))))." elle sera livrer au nom de ".$_REQUEST["nom"]." a l'adresse ".$_REQUEST["adresse"];
+//echo 'la livraison de votre commande est estimer a  '.date('H:i:s', strtotime('+30 minutes', strtotime(date('H:i:s'))))." elle sera livrer au nom de ".$_REQUEST["nom"]." a l'adresse ".$_REQUEST["adresse"];
 
 $infoscommande = "\nnom et prenom :".$_REQUEST['nomprenom'].", email :".$_REQUEST['Email'].", telephone :".$_REQUEST['tel'].", adresse du client :".$_REQUEST['adresse'].", date et heure de la commande :".date("d/m/Y H-m-s");
 
